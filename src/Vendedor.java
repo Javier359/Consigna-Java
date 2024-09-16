3ceff5e9cd3f20d204da054c0b603f662afc9619
Javier Jimenez <javier.sniguel@gmail.com>
2024-09-16 19:26:55 -0300

    se agrego la clase Venta y Vendedor

---
+++ src/Vendedor.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vendedor {
+    private String codigo;
+    private String nombre;
+    private double sueldo;
+    private List<Venta> ventas;
+
+    public Vendedor(String codigo, String nombre, double sueldo) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+        this.ventas = new ArrayList<>();
+    }
+
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+}
