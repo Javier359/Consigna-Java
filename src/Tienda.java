05eaa20365d006ae125aae57c87754ce7019b50a
Javier Jimenez <javier.sniguel@gmail.com>
2024-09-16 20:37:34 -0300

    se creo la clase tienda que relaciona cada venta con sun vendedor y el producto vendido con el metodo registrarVenta

---
+++ src/Tienda.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tienda {
+    private List<Producto> productos;
+    private List<Vendedor> vendedores;
+
+    public Tienda() {
+        productos = new ArrayList<>();
+        vendedores = new ArrayList<>();
+    }
+
+    //metodos propios de logica de relacion de productos con vendedores
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+    public void agregarVendedor(Vendedor vendedor) {
+        vendedores.add(vendedor);
+    }
+
+    public void registrarVenta(String codigoProducto, String codigoVendedor) throws Exception {
+
+        Producto producto = null;
+        Vendedor vendedor = null;
+        //buscamos producto
+        for(Producto p : productos) {
+            if(p.getCodigo().equals(codigoProducto)) {
+                producto = p;
+                break;
+            }
+        }
+        //buscamos vendedor
+        for(Vendedor v : vendedores){
+            if(v.getCodigo().equals(codigoVendedor)){
+                vendedor = v;
+                break;
+            }
+        }
+        if(producto == null || vendedor == null){
+            throw new Exception("No existe un producto o vendedor dicho codigo");
+        }
+        Venta venta = new Venta(producto, vendedor);
+        vendedor.agregarVenta(venta);
+    }
+}
